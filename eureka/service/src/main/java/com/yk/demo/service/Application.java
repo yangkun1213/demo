c98bedc612f4de2d1026291834718a91e467af61
yangkun <18301117840@163.com>
2019-12-26 17:49:27 +0800

    1.调整build.gradle.

--- eureka/service/src/main/java/com/yk/demo/service/Application.java
+++ eureka/service/src/main/java/com/yk/demo/service/Application.java
@@ -1,5 +1,6 @@
 package com.yk.demo.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +10,22 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
 
+
+
+
+    private static Logger logger = Logger.getLogger(Application.class);
+
+    /**
+     * 主启动方法
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+
+        logger.info("======spring boot start success ===========");
+
     }
+
+
 }
